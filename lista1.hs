cb7c7d8286e3895b74a98f4be11f882475344752
Caio Arruda <caio.arruda@pagar.me>
2022-06-01 21:10:54 -0300

    Adds list comprehensions examples

--- lista1.hs
+++ lista1.hs
@@ -16,3 +16,26 @@ isEven x = if mod x 2 == 0 then True else False
 
 isEven' x | mod x 2 == 0 = True 
          | otherwise = False
+
+-- s indicates list params
+funnyWithLists xs = 'A':xs 
+
+-- Gets element in list by index, use !!
+getsElementByIndex xs = xs !! 2
+
+----------- Learning list comprehensions ------------
+comprehensions x = [x * 3 | x <- [1..10]] 
+
+comprehensions' x = [x * 3 | x <- [1..5], x*3 >= 9]
+
+-- elemento | range | condição
+-- [x | x <- range | condition]
+boomBang xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+forEach xs = [ x | x <- xs, even x]
+
+
+forEach' xs = map square xs 
+
+-- TODO: Estudar pattern matching e guards
+-- https://devtut.github.io/haskell/list-comprehensions.html#basic-list-comprehensions
